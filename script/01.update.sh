fee0a057e5266a08a52df5a005220b2b25537696
shinyay <shinya.com@gmail.com>
2020-05-05 18:54:45 +0900

    Add repositry list for Debian

--- script/01.update.sh
+++ script/01.update.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+sudo echo "deb http://ftp.jp.debian.org/debian stretch main contrib non-free" >> /etc/apt/sources.list
+sudo echo "deb http://ftp.jp.debian.org/debian stretch-updates main contrib non-free" >> /etc/apt/sources.list
 sudo apt -y update
 sudo apt -y upgrade
